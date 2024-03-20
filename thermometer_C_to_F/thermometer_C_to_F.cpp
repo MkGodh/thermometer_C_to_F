8f37a2ea9b54eb9812c42f6de18f7e754c49db51
MkGodh <fenryrvla@yandex.ru>
2024-03-20 20:36:48 +0300

    Добавьте файлы проекта.

---
+++ thermometer_C_to_F/thermometer_C_to_F.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+using namespace std;
+setlocale(LC_ALL, "RUS");
+
+int lowerBorder;
+int upperBorder;
+int step;
+
+cout << "Ввод: \n" << "Нижняя граница: "; cin >> lowerBorder;
+cout << "Верхняя граница: "; cin >> upperBorder;
+cout << "Шаг: "; cin >> step;
+
+cout << "Вывод: \n" << "C " << "   " << "F" << "\n";
+while (lowerBorder <= upperBorder) {
+    float zeroHero = 32; // 32 68
+    if (lowerBorder == 0) {
+        
+        cout << lowerBorder << "   " << 32 << "\n";
+    }
+    lowerBorder += step; // 20 // 40
+    zeroHero = ((float)lowerBorder * 1.8) + zeroHero;
+    cout << lowerBorder << "   " << zeroHero << "\n";
+
+
+
+}
+}
+
